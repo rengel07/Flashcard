d40308a42eecd7a668047350fbd5fd821f809a8f
rengel07 <146237760+rengel07@users.noreply.github.com>
2024-11-12 10:07:45 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'caminhão',
+    'Qual é o caminhão mais forte do mundo?',
+    'O caminhão mais forte do mundo é o Shacman X6000'
+    )
+    
+    criaCartao(
+    'caminhão',
+    'Qual é a cavalaria dele?',
+    'Ele tem um motor v8 de 800cv.'
+    )
+    
+    criaCartao(
+    'caminhão',
+    'Qual é o nome do seu criador?',
+    'O seu criador é a empresa Fast Gear'
+    )
+    
+    criaCartao(
+    'caminhão',
+    'Qual é o modelo do cambio?',
+    'O cambio é o S16AD de 16 velocidades'
+    )
